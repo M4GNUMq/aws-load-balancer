b0530477ab8d5ddfbef8171288530b3404de6797
Omer Edry <omere335@gmail.com>
2026-05-19 00:19:54 +0300

    allow only amazon AMIs and validate subnet is public

--- main.tf
+++ main.tf
@@ -4,9 +4,21 @@ provider "aws" {
 
 data "aws_ami" "load_balancer" {
   most_recent = true
+  owners      = ["amazon"]
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = ["vpc-0a86b5aeb59d541df"]
+  }
+  filter {
+    name   = "map-public-ip-on-launch"
+    values = ["true"]
   }
 }
 
@@ -14,12 +26,14 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "6.4.0"
 
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  name                   = "load-balancer"
-  ami                    = data.aws_ami.load_balancer.id
-  vpc_security_group_ids = [aws_security_group.load_balancer.id]
-  user_data              = templatefile("${path.module}/initLB.sh", {
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  name                        = "load-balancer"
+  ami                         = data.aws_ami.load_balancer.id
+  subnet_id                   = data.aws_subnets.public.ids[0]
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.load_balancer.id]
+  user_data                   = templatefile("${path.module}/initLB.sh", {
     web_server_count = var.web_server_count
   })
 
